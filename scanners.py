b9073458a49984e6d5630e08d02dccecbb1f94a5
Sidney Dodson <siddodson@gmail.com>
2018-09-24 12:42:08 -1000

    Add multianalysis function

--- scanners.py
+++ scanners.py
@@ -33,14 +33,23 @@ with fd as reader :
       total_scans += 1
 
 
+# create dictionary that tracks total hits per origin IP Address
 total_access_attempts = copy.deepcopy(master_obj)
-
-# print(master_obj)
-
 for each in total_access_attempts:
   total_access_attempts[each] = len(total_access_attempts[each])
-
 print(total_access_attempts)
+
+# create dictionary that tracks unique hits per origin IP Address
+unique_dest = copy.deepcopy(master_obj)
+for each in unique_dest:
+  unique_dest[each] = list(set(unique_dest[each]))
+# print(unique_dest)
+
+# create dictionary that tracks total unique addresses that origin hits
+unique_totals = copy.deepcopy(unique_dest)
+for each in unique_totals:
+  unique_totals[each] = len(unique_totals[each])
+# print(unique_totals)
 
 # now that each line is a list within a super list, organize all Addresses
 all_origins = []
@@ -76,6 +85,10 @@ for each in all_destination:
 # print('\n[scan destination hosts] ')
 # for each in all_destination:
 #   print(each)
+
+
+append_file.write('\nCONCLUSION\n')
+append_file.write('\n It appears that \n')
 
 
 append_file.close()
