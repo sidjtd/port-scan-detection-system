b6100ea262332fd6784db00c9fb93be4625fc92b
Sidney Dodson <siddodson@gmail.com>
2018-09-23 22:47:28 -1000

    Split each line to separate

--- scanners.py
+++ scanners.py
@@ -1,9 +1,22 @@
+# requirements
 import os
 import re
 
+# hard-coded parth to log file
 filePath = "./ssh.log.txt"
-fd = open(filePath, 'r')
 
+# opening up filePath file in read mode
+fd = open(filePath, 'r')
+total_scans = 0;
+
+all_split_stored = []
+
+# prints out all lines in file
 with fd as reader :
-    for line in reader :
-        print( line )
+  for line in reader :
+    all_split_stored.append(line.split())
+    total_scans += 1
+
+print('[scan attempts] ', total_scans)
+print('[test] ', all_split_stored)
+
