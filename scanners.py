b6698409952743a3d566c1f34b961d9916bc2f64
Sidney Dodson <siddodson@gmail.com>
2018-09-24 12:34:10 -1000

    Added Access attempts

--- scanners.py
+++ scanners.py
@@ -1,7 +1,7 @@
 # requirements
 import os
 import re
-
+import copy
 # hard-coded parth to log file
 filePath = "./ssh.log.txt"
 
@@ -16,11 +16,31 @@ master_obj = {}
 with fd as reader :
   for line in reader :
     split_up = line.split()
-    if(len(split_up) > 0):
+
+    # if scan line has more than 4 elements, do the following
+    if(len(split_up) > 4):
+      key_origin = '{}'.format(split_up[2])
+
+      # if key doesnt exist, make it a list. If already list, just add value to lists
+      if key_origin in master_obj:
+        master_obj[key_origin].append(split_up[4])
+      else:
+        master_obj[key_origin] = []
+        master_obj[key_origin].append(split_up[4])
+
+      # prepare to create duplicate free list
       all_split_stored.append(split_up)
       total_scans += 1
 
 
+total_access_attempts = copy.deepcopy(master_obj)
+
+# print(master_obj)
+
+for each in total_access_attempts:
+  total_access_attempts[each] = len(total_access_attempts[each])
+
+print(total_access_attempts)
 
 # now that each line is a list within a super list, organize all Addresses
 all_origins = []
